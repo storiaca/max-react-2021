9b1b26f3007a385ed8b0e76095e454f52691c98b
Aleksandar Ristic <storiaca@gmail.com>
2021-05-08 13:53:55 +0200

    Managing the Auth state with context

--- 022-auth-in-react/src/components/Auth/AuthForm.js
+++ 022-auth-in-react/src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
@@ -7,6 +8,8 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -54,7 +57,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         alert(err.message);
