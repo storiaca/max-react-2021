7696b81d7469c3b507a43330f74a6d7befc0da4f
Aleksandar Ristic <storiaca@gmail.com>
2021-05-22 14:29:06 +0200

    Form validation start

--- 016-forms-userinput/src/components/SimpleInput.js
+++ 016-forms-userinput/src/components/SimpleInput.js
@@ -1,9 +1,17 @@
+import { useRef, useState } from "react";
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  console.log(enteredName);
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
