7f04269df9dfef8ae715e23a125ce26e6fae9ce1
Aleksandar Ristic <storiaca@gmail.com>
2021-08-05 17:08:12 +0200

    Firebase start

--- 014-http-db-connection/src/App.js
+++ 014-http-db-connection/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
-
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
+// start implement firebase
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -13,9 +13,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
---
+++ 014-http-db-connection/src/components/ActivateUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ActivateUser = () => {
+  return (
+    <div>
+      <h3>Activate User</h3>
+    </div>
+  );
+};
+
+export default ActivateUser;
