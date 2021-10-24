168addce3e8c0c634be30981e29c1d9b0d48be5a
Aleksandar Ristic <storiaca@gmail.com>
2021-10-24 19:13:41 +0200

    010-Input comonent start

--- 010-use-effect-reducer-context/src/components/Login/Login.js
+++ 010-use-effect-reducer-context/src/components/Login/Login.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -88,20 +89,7 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
+        <Input id="email" label="E Mail" type="email" isValid={emailIsValid} />
         <div
           className={`${classes.control} ${
             passwordState.isValid === false ? classes.invalid : ""
