23458160834c3d38d887e196f3c3656ab4cfa08f
Aleksandar Ristic <storiaca@gmail.com>
2021-11-26 12:37:57 +0100

    Add Card to AvailableMeals

--- 011-food-order-app/src/components/Meals/AvailableMeals.js
+++ 011-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -34,7 +34,9 @@ const AvailableMeals = () => {
   ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
--- 011-food-order-app/src/components/UI/Card.js
+++ 011-food-order-app/src/components/UI/Card.js
@@ -1,11 +1,7 @@
 import React from "react";
 import classes from "./Card.module.css";
-const Card = () => {
-  return (
-    <div>
-      <h3>Card</h3>
-    </div>
-  );
+const Card = (props) => {
+  return <div className={classes.card}>{props.children}</div>;
 };
 
 export default Card;
