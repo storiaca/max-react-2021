367b8d339a938af4fc43c45440abb75de8d5e771
Aleksandar Ristic <storiaca@gmail.com>
2021-12-04 14:06:03 +0100

    Adding a form

--- 011-food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ 011-food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,20 @@
 import React from "react";
-import classes from "MealItemForm.module.css";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 const MealItemForm = () => {
   return (
-    <form>
-      <input />
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
