8b6d18c9410a27c8cf7b72db571adc5d4aee741f
Aleksandar Ristic <storiaca@gmail.com>
2021-12-29 17:43:56 +0100

    017-Food order http form start

--- 017-food-order-http-form/src/components/Layout/HeaderCartButton.js
+++ 017-food-order-http-form/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
-import CartIcon from "../Cart/CartIcon";
+import { useContext, useEffect, useState } from 'react';
 
-import CartContext from "../../store/cart-context";
-import classes from "./HeaderCartButton.module.css";
+import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
+
 const HeaderCartButton = (props) => {
-  const [btnIsHiglighted, setBtnIsHighlighted] = useState(false);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+
   const { items } = cartCtx;
 
-  const numberOfCartItems = items.reduce((currNumber, item) => {
-    return currNumber + item.amount;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
   }, 0);
 
-  const btnClasses = `${classes.button} ${btnIsHiglighted ? classes.bump : ""}`;
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
   useEffect(() => {
     if (items.length === 0) {
