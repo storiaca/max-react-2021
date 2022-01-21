0070e6cb19af74f94343155d1cf662d7e92319c7
Aleksandar Ristic <storiaca@gmail.com>
2022-01-21 17:55:56 +0100

    Submitting and sending cart data finish

--- 017-food-order-http-form/src/components/Cart/Cart.js
+++ 017-food-order-http-form/src/components/Cart/Cart.js
@@ -25,7 +25,18 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
-  const submitOrderHandler = (userData) => {};
+  const submitOrderHandler = (userData) => {
+    fetch(
+      "https://max-reat-http-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
