973c258713e58d41ce3919f542cb78a15ff51004
Aleksandar Ristic <storiaca@gmail.com>
2022-01-27 17:40:26 +0100

    Defining and using routes finished

--- 020-multi-page-spa/src/App.js
+++ 020-multi-page-spa/src/App.js
@@ -1,8 +1,15 @@
 import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
