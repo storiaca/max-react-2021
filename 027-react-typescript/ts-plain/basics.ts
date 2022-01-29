2665c5d45b776b6042c02d44fdebbb0d44f1cb6b
Aleksandar Ristic <storiaca@gmail.com>
2022-01-29 19:54:09 +0100

    Exploring the base types

---
+++ 027-react-typescript/ts-plain/basics.ts
@@ -0,0 +1,17 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let username: string;
+
+username = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
