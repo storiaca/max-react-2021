b8113d05cd539fb0a0c5f7783b09c2a0bb9a89f7
Aleksandar Ristic <storiaca@gmail.com>
2022-01-30 14:19:23 +0100

    Working with array and obejct types

--- 027-react-typescript/ts-plain/basics.ts
+++ 027-react-typescript/ts-plain/basics.ts
@@ -15,3 +15,27 @@ username = "Max";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+// More complex types
+let hobbies: string[];
+
+hobbies = ['Sports', "Cooking"];
+
+let person: {
+  name: string,
+  age: number
+};
+
+person = {
+  name: "Max",
+  age: 32
+}
+
+// person = {
+//  isEmployee: true
+// }
+
+let people: {
+  name: string,
+  age: number
+}[];
