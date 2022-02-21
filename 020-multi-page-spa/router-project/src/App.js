98c33de571cf0213902becee67f23822c9e64b0f
Aleksandar Ristic <storiaca@gmail.com>
2022-02-21 16:45:48 +0100

    Add nested route for comments

--- 020-multi-page-spa/router-project/src/App.js
+++ 020-multi-page-spa/router-project/src/App.js
@@ -1,10 +1,23 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetails from "./pages/QuoteDetails";
+import NewQuote from "./pages/NewQuote";
 function App() {
   return (
     <Switch>
-      <Route path="/qoutes"></Route>
-      <Route path="/qoutes/:qouteId"></Route>
-      <Route path="/new-quote"></Route>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:qouteId">
+        <QuoteDetails />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
     </Switch>
   );
 }
--- 020-multi-page-spa/router-project/src/pages/QuoteDetails.js
+++ 020-multi-page-spa/router-project/src/pages/QuoteDetails.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useParams, Route } from "react-router-dom";
 
+import Comments from "../components/comments/Comments";
 const QuoteDetails = () => {
+  const params = useParams();
   return (
     <div>
       <h2>QuoteDetails</h2>
+      <p>{params.qouteId}</p>
+      <Route path={`/quotes/${params.qouteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
   );
 };
