8dea6cf44a48a6df1b93b5619fdae63538ab3cb4
Aleksandar Ristic <storiaca@gmail.com>
2022-03-07 16:05:04 +0100

    Add env file

--- 014-http-db-connection/src/App.js
+++ 014-http-db-connection/src/App.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
+  const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE_DOMAIN;
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback(async () => {
+  const fetchMovieHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(
-        "https://max-reat-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
-      );
+      const response = await fetch(`${FIREBASE_DOMAIN}/movies.json`);
+
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -34,37 +34,42 @@ function App() {
       }
 
       setMovies(loadedMovies);
+      setIsLoading(false);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }, []);
+  }, [FIREBASE_DOMAIN]);
 
   useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
 
   async function addMovieHandler(movie) {
-    const response = await fetch(
-      "https://max-reat-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
-      {
+    try {
+      const response = await fetch(`${FIREBASE_DOMAIN}/movies.json`, {
         method: "POST",
         body: JSON.stringify(movie),
         headers: {
           "Content-Type": "application/json",
         },
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
       }
-    );
-    const data = await response.json();
-    console.log(data);
+
+      const data = await response.json();
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
-  let content = <p>Found no movies.</p>;
+  let content = <p>Found no movies</p>;
 
-  if (movies.length > 0) {
+  if (!isLoading && movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
-
   if (error) {
     content = <p>{error}</p>;
   }
@@ -79,7 +84,7 @@ function App() {
         <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
     </React.Fragment>
