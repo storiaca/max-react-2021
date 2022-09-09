63f4576314b0db755ee451ffcdd5ba2518e49736
Aleksandar Ristic <storiaca@gmail.com>
2022-09-09 09:20:05 +0200

    Handling http states and feedback with redux finished

--- 019-advanced-redux/src/App.js
+++ 019-advanced-redux/src/App.js
@@ -1,16 +1,29 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE;
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  let isInitial = true;
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
       const response = await fetch(`${FIREBASE_DOMAIN}cart.json`, {
         method: "PUT",
         body: JSON.stringify(cart),
@@ -20,15 +33,45 @@ function App() {
         throw new Error("Sending cart data failed!");
       }
 
-      const responseData = await response.json();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success...",
+          message: "Sent cart data succesfully",
+        })
+      );
     };
-  }, [cart, FIREBASE_DOMAIN]);
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error...",
+          message: "Sending cart data failed",
+        })
+      );
+    });
+  }, [cart, FIREBASE_DOMAIN, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          message={notification.message}
+          title={notification.title}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
