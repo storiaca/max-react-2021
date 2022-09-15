39127d2990e046c71baf7daafd25de9219fdd0d5
Aleksandar Ristic <storiaca@gmail.com>
2022-09-15 16:40:29 +0200

    Fix fecth bug

--- 019-advanced-redux/src/store/cart-actions.js
+++ 019-advanced-redux/src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -45,7 +50,10 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(`${FIREBASE_DOMAIN}/cart.json`, {
         method: "PUT",
-        body: JSON.stringify(cart),
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity,
+        }),
       });
 
       if (!response.ok) {
