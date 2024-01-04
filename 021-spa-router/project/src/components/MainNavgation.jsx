4a4fb37580a13c678ac1df7b3ab2a51e4b293351
Aleksandar Ristic <storiaca@gmail.com>
2024-01-04 11:54:17 +0100

    Worknig with navigation links (NavLink)

--- 021-spa-router/project/src/components/MainNavgation.jsx
+++ 021-spa-router/project/src/components/MainNavgation.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 const MainNavgation = () => {
   return (
@@ -6,10 +6,28 @@ const MainNavgation = () => {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              // style={({ isActive }) => ({
+              //   textAlign: isActive ? "center" : "left",
+              // })}
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
