1bb5f6b33858f8d7d3ea07d5bff35ab2fb7000a5
Aleksandar Ristic <storiaca@gmail.com>
2024-01-16 15:40:57 +0100

    Extracting error data and throwing responses second part

--- 021-spa-router/adv-project/frontend/src/pages/Error.jsx
+++ 021-spa-router/adv-project/frontend/src/pages/Error.jsx
@@ -1,10 +1,28 @@
+import { useRouteError } from "react-router-dom";
 import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
+  let title = "An error occured";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
   return (
-    <PageContent title="An error occured">
-      <p>Something went wrong!</p>
-    </PageContent>
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
   );
 };
 
