39d19f8df8d50f5e02ba5d4b4da45c41a7d68252
Aleksandar Ristic <storiaca@gmail.com>
2024-01-23 22:54:46 +0100

    Add new function to http.js

--- 024-tanstack/project/src/util/http.js
+++ 024-tanstack/project/src/util/http.js
@@ -61,3 +61,18 @@ export async function fetchSelectableImages({ signal }) {
 
   return images;
 }
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
