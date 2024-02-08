4cc2b232ccec01c775a1d5790b6b26105dce9d00
Aleksandar Ristic <storiaca@gmail.com>
2024-02-08 12:34:37 +0100

    Fresh start for 003 section

--- 003-react-essentials/src/App.jsx
+++ 003-react-essentials/src/App.jsx
@@ -1,73 +1,18 @@
-import { useRef, useState } from 'react';
-
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-
 function App() {
-  const modal = useRef();
-  const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
-
-  function handleStartRemovePlace(id) {
-    modal.current.open();
-    selectedPlace.current = id;
-  }
-
-  function handleStopRemovePlace() {
-    modal.current.close();
-  }
-
-  function handleSelectPlace(id) {
-    setPickedPlaces((prevPickedPlaces) => {
-      if (prevPickedPlaces.some((place) => place.id === id)) {
-        return prevPickedPlaces;
-      }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
-      return [place, ...prevPickedPlaces];
-    });
-  }
-
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    modal.current.close();
-  }
-
   return (
-    <>
-      <Modal ref={modal}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
-
+    <div>
       <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
         <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
         </p>
       </header>
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        <Places
-          title="Available Places"
-          places={AVAILABLE_PLACES}
-          onSelectPlace={handleSelectPlace}
-        />
+        <h2>Time to get started!</h2>
       </main>
-    </>
+    </div>
   );
 }
 
