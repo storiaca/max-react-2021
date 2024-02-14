f8dbd37347c2e98a3df986091454470f9bcaaa45
Aleksandar Ristic <storiaca@gmail.com>
2024-02-14 18:04:05 +0100

    Component composition, special children prop

--- 003-react-essentials/src/App.jsx
+++ 003-react-essentials/src/App.jsx
@@ -1,13 +1,14 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <div id="core-concepts">
+        <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((core) => (
@@ -19,7 +20,16 @@ function App() {
               />
             ))}
           </ul>
-        </div>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
