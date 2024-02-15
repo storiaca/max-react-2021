20da0901eea3faac98cd89feb79a64a66355c332
Aleksandar Ristic <storiaca@gmail.com>
2024-02-15 11:08:28 +0100

    CSS styling and dynamic styling

--- 003-react-essentials/src/components/TabButton.jsx
+++ 003-react-essentials/src/components/TabButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const TabButton = ({ children, onSelect }) => {
+const TabButton = ({ children, onSelect, isSelected }) => {
+  console.log(children);
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 };
