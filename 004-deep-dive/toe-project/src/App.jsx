85fe483fd4bdf973a91b9996acda9dd984e0c2e6
Aleksandar Ristic <storiaca@gmail.com>
2024-02-21 19:01:38 +0100

    Rendering multi dimensional lists

--- 004-deep-dive/toe-project/src/App.jsx
+++ 004-deep-dive/toe-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 function App() {
   return (
     <main>
@@ -7,7 +8,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
