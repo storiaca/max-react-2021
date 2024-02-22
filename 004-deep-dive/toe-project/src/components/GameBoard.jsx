e02c7b323a07e0950aad87f932fb8bc7b952f1dd
Aleksandar Ristic <storiaca@gmail.com>
2024-02-22 11:53:12 +0100

    Updating object state immutably

--- 004-deep-dive/toe-project/src/components/GameBoard.jsx
+++ 004-deep-dive/toe-project/src/components/GameBoard.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+// setGrid((prevGameBoard) => {
+//   const updatedBoard = structuredClone(prevGameBoard)
+//   updatedBoard[x][y] = value
+//   return updatedBoard
+// })
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -7,14 +13,28 @@ const initialGameBoard = [
 ];
 
 const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray])
+      ];
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
