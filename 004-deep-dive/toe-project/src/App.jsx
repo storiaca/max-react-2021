f61a36dde9ec5aaafc4d97015afc8abf7ae5ef4b
Aleksandar Ristic <storiaca@gmail.com>
2024-02-24 10:03:59 +0100

    Reducing state management and identifying unnecessary state

--- 004-deep-dive/toe-project/src/App.jsx
+++ 004-deep-dive/toe-project/src/App.jsx
@@ -1,20 +1,36 @@
 import { useState } from 'react';
+
 import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
-    setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
 
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+    let currentPlayer = 'X';
+
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+      currentPlayer = 'O';
+    }
+
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -25,7 +41,6 @@ function App() {
     });
   }
 
-  console.log(gameTurns);
   return (
     <main>
       <div id="game-container">
