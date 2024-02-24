816a02ccd4549606e037767eff0e2216acbac647
Aleksandar Ristic <storiaca@gmail.com>
2024-02-24 10:20:22 +0100

    Outsourcing data into a separate file

--- 004-deep-dive/toe-project/src/App.jsx
+++ 004-deep-dive/toe-project/src/App.jsx
@@ -4,6 +4,8 @@ import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 
+import { WINNING_COMBINATIONS } from './winning-combinations';
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
