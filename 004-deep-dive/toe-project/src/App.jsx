dc99958344fbba77f8a1f935a934812a5ad01f5a
Aleksandar Ristic <storiaca@gmail.com>
2024-02-25 19:44:55 +0100

    An alternative to lifting state up

--- 004-deep-dive/toe-project/src/App.jsx
+++ 004-deep-dive/toe-project/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -108,11 +108,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
--- 004-deep-dive/toe-project/src/components/Player.jsx
+++ 004-deep-dive/toe-project/src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
-const Player = ({ initialName, symbol, isActive }) => {
+const Player = ({ initialName, symbol, isActive, onChangeName }) => {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChangePlayer(e) {
