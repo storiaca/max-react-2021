891edea21295f60d6c353acbe96d5e5609116b38
Aleksandar Ristic <storiaca@gmail.com>
2024-02-27 09:33:55 +0100

    Fix code

--- 004-deep-dive/toe-project/src/App.jsx
+++ 004-deep-dive/toe-project/src/App.jsx
@@ -30,8 +30,8 @@ function deriveActivePlayer(gameTurns) {
 
 function deriveGameBoard(gameTurns) {
   // deep clone example with map
-  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
-  //let gameBoard = structuredClone(INITIAL_GAME_BOARD);
+  // let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])]
+  let gameBoard = structuredClone(INITIAL_GAME_BOARD);
 
   // if turns are empty array this for loop won't execute
   for (const turn of gameTurns) {
@@ -68,7 +68,7 @@ function deriveWinner(gameBoard, players) {
 }
 
 function App() {
-  const [players, setPlayers] = useState([PLAYERS]);
+  const [players, setPlayers] = useState(PLAYERS);
   const [gameTurns, setGameTurns] = useState([]);
   // const [activePlayer, setActivePlayer] = useState('X');
 
@@ -142,3 +142,148 @@ function App() {
 }
 
 export default App;
+
+// import { useState } from 'react';
+
+// import Player from './components/Player';
+// import GameBoard from './components/GameBoard';
+// import Log from './components/Log';
+// import GameOver from './components/GameOver';
+
+// import { WINNING_COMBINATIONS } from './winning-combinations';
+
+// const PLAYERS = {
+//   X: 'Player 1',
+//   O: 'Player 2'
+// };
+
+// const INITIAL_GAME_BOARD = [
+//   [null, null, null],
+//   [null, null, null],
+//   [null, null, null]
+// ];
+
+// function deriveActivePlayer(gameTurns) {
+//   let currentPlayer = 'X';
+
+//   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+//     currentPlayer = 'O';
+//   }
+
+//   return currentPlayer;
+// }
+
+// function deriveGameBoard(gameTurns) {
+//   // deep clone example with map
+//   let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
+//   //let gameBoard = structuredClone(INITIAL_GAME_BOARD);
+
+//   // if turns are empty array this for loop won't execute
+//   for (const turn of gameTurns) {
+//     const { square, player } = turn;
+//     const { row, col } = square;
+
+//     gameBoard[row][col] = player;
+//   }
+
+//   return gameBoard;
+// }
+
+// function deriveWinner(gameBoard, players) {
+//   let winner;
+
+//   for (const combination of WINNING_COMBINATIONS) {
+//     const firstSquareSymbol =
+//       gameBoard[combination[0].row][combination[0].column];
+//     const secondSquareSymbol =
+//       gameBoard[combination[1].row][combination[1].column];
+//     const thirdSquareSymbol =
+//       gameBoard[combination[2].row][combination[2].column];
+
+//     if (
+//       firstSquareSymbol &&
+//       firstSquareSymbol === secondSquareSymbol &&
+//       firstSquareSymbol === thirdSquareSymbol
+//     ) {
+//       winner = players[firstSquareSymbol];
+//     }
+//   }
+
+//   return winner;
+// }
+
+// function App() {
+//   const [players, setPlayers] = useState([PLAYERS]);
+//   const [gameTurns, setGameTurns] = useState([]);
+//   // const [activePlayer, setActivePlayer] = useState('X');
+
+//   const activePlayer = deriveActivePlayer(gameTurns);
+
+//   const gameBoard = deriveGameBoard(gameTurns);
+
+//   const winner = deriveWinner(gameBoard, players);
+
+//   const hasDraw = gameTurns.length === 9 && !winner;
+
+//   function handleRestart() {
+//     setGameTurns([]);
+//   }
+
+//   function handlePlayerNameChange(symbol, newName) {
+//     setPlayers((prevPlayers) => {
+//       return {
+//         ...prevPlayers,
+//         [symbol]: newName
+//       };
+//     });
+//   }
+
+//   function handleSelectSquare(rowIndex, colIndex) {
+//     // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+
+//     let currentPlayer = 'X';
+
+//     if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+//       currentPlayer = 'O';
+//     }
+
+//     setGameTurns((prevTurns) => {
+//       const currentPlayer = deriveActivePlayer(prevTurns);
+
+//       const updatedTurns = [
+//         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+//         ...prevTurns
+//       ];
+
+//       return updatedTurns;
+//     });
+//   }
+
+//   return (
+//     <main>
+//       <div id="game-container">
+//         <ol id="players" className="highlight-player">
+//           <Player
+//             initialName={PLAYERS.X}
+//             symbol="X"
+//             isActive={activePlayer === 'X'}
+//             onChangeName={handlePlayerNameChange}
+//           />
+//           <Player
+//             initialName={PLAYERS.O}
+//             symbol="O"
+//             isActive={activePlayer === 'O'}
+//             onChangeName={handlePlayerNameChange}
+//           />
+//         </ol>
+//         {(winner || hasDraw) && (
+//           <GameOver winner={winner} onRestart={handleRestart} />
+//         )}
+//         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
+//       </div>
+//       <Log turns={gameTurns} />
+//     </main>
+//   );
+// }
+
+// export default App;
