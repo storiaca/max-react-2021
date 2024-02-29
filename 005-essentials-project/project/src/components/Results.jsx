3c26098f1c682b3a1af3425e07b5e40569dc77f6
Aleksandar Ristic <storiaca@gmail.com>
2024-02-29 17:24:12 +0100

    Section 08 Refs and portals

--- 005-essentials-project/project/src/components/Results.jsx
+++ 005-essentials-project/project/src/components/Results.jsx
@@ -4,7 +4,24 @@ import { calculateInvestmentResults } from "../util/investment";
 const Results = ({ userInput }) => {
   const resultsData = calculateInvestmentResults(userInput);
   console.log(resultsData);
-  return <div>Results..{userInput.initialInvestment}</div>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
 };
 
 export default Results;
