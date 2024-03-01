94f0f572b203e4bb445783520dfbdb7fb95e5784
Aleksandar Ristic <storiaca@gmail.com>
2024-03-01 16:15:49 +0100

    Outputting results in a list and deriving more values

--- 005-essentials-project/project/src/components/Results.jsx
+++ 005-essentials-project/project/src/components/Results.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 const Results = ({ userInput }) => {
   const resultsData = calculateInvestmentResults(userInput);
-  console.log(resultsData);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+
   return (
     <table id="result">
       <thead>
@@ -16,9 +20,23 @@ const Results = ({ userInput }) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td></td>
-        </tr>
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            (yearData.annualInvestment * yearData.year - initialInvestment);
+
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
