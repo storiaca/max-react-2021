e7e26995fe3fc02f20443372c4fcbc3582a99a69
Aleksandar Ristic <storiaca@gmail.com>
2024-03-13 13:12:44 +0100

    using refs for more then dome element connections

--- 008-ref-portals/project/src/components/TimerChallenge.jsx
+++ 008-ref-portals/project/src/components/TimerChallenge.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
 
     setTimerStarted(true);
   }
 
-  function handleStop() {}
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
 
   return (
     <section className="challenge">
@@ -22,7 +26,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
