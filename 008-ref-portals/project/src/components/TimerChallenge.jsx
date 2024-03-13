a82f9ee2ec7b8792ff5b580538a97a11c08e888a
Aleksandar Ristic <storiaca@gmail.com>
2024-03-13 10:18:01 +0100

    Adding challenges to demo project

---
+++ 008-ref-portals/project/src/components/TimerChallenge.jsx
@@ -0,0 +1,16 @@
+const TimerChallenge = ({ title, targetTime }) => {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="active">Time is running / Timer inactive</p>
+    </section>
+  );
+};
+
+export default TimerChallenge;
