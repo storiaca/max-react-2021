06ae4641146076803c7f3469a4d5591ca9bacc26
Aleksandar Ristic <storiaca@gmail.com>
2024-04-01 15:24:54 +0200

    Adding Newproject an Input component

--- 009-practice/src/App.jsx
+++ 009-practice/src/App.jsx
@@ -1,9 +1,10 @@
+import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
-
 function App() {
   return (
-    <main className="h-screen">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
---
+++ 009-practice/src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input({ label, textarea, ...props }) {
+  return (
+    <p>
+      <label>{label}</label>
+      {textarea ? <textarea {...props} /> : <input {...props} />}
+    </p>
+  );
+}
+
+export default Input;
