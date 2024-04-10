9d80bb7b43632b1bcdd2a7584dc9d41b6c773302
Aleksandar Ristic <storiaca@gmail.com>
2024-04-10 13:31:04 +0200

    Managing state to swithc between components

--- 009-practice/src/components/NoProjectSelected.jsx
+++ 009-practice/src/components/NoProjectSelected.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Button from "./Button";
+
 import imageProject from "../assets/no-projects.png";
-function NoProjectSelected() {
+
+function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -16,7 +18,7 @@ function NoProjectSelected() {
         Select a project or get started with a new one
       </p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
