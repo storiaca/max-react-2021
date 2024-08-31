311c39737d6b93b8a35d0d30cb8903ac07990fab
Aleksandar Ristic <storiaca@gmail.com>
2024-08-31 11:46:29 +0200

    Not all side effects need useeffect

--- 011-side-effects/src/App.jsx
+++ 011-side-effects/src/App.jsx
@@ -41,6 +41,15 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    // check if there is item with same id
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
