fe6ee4b75083859ad5cfbe9e9f055a37cf60a2a6
Aleksandar Ristic <storiaca@gmail.com>
2024-09-05 17:31:47 +0200

    Preparing another problem taht can be fixed with useEffect

--- 011-side-effects/src/components/DeleteConfirmation.jsx
+++ 011-side-effects/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,10 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  console.log("Timer set");
+
+  setTimeout(() => {
+    onConfirm();
+  }, 3000);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
