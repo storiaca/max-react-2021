c5483f673a2fc3d60f98c26199e3bd958dfd5ee2
Aleksandar Ristic <storiaca@gmail.com>
2024-09-11 11:20:50 +0200

    Working with effect dependencies and useCallback

--- 012-quiz-app/src/components/QuestionTimer.jsx
+++ 012-quiz-app/src/components/QuestionTimer.jsx
@@ -4,10 +4,13 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
   const [reamainigTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
+    console.log("Setting Timeout");
+
     setTimeout(onTimeout, timeout);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
+    console.log("Setting Interval");
     setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
--- 012-quiz-app/src/components/Quiz.jsx
+++ 012-quiz-app/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QuestionTimer from "./QuestionTimer.jsx";
 
 import QUESTIONS from "../questions.js";
@@ -10,11 +10,19 @@ const Quiz = () => {
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
     return (
@@ -31,10 +39,7 @@ const Quiz = () => {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer
-          timeout={10000}
-          onTimeout={() => handleSelectAnswer(null)}
-        />
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
