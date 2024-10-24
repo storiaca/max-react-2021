ddee541c1a1bd1ba6109a734c601fa01912276df
Aleksandar Ristic <storiaca@gmail.com>
2024-10-24 15:06:30 +0200

    Transforming fetched data

--- 015-db-connect/src/components/AvailablePlaces.jsx
+++ 015-db-connect/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import ErrorPage from "./Error.jsx";
 import Places from "./Places.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -17,15 +18,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if (!response.ok) {
           throw new Error("Failed to fetch places");
         }
-        setAvailablePlaces(resData.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || "Could not fetch places, please try again later",
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
