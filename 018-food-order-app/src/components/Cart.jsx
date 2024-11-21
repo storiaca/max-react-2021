15210a8cdb50d350d0345bc4a23eb2053500de08
Aleksandar Ristic <storiaca@gmail.com>
2024-11-21 11:15:45 +0100

    Opening the Cart in the Modal via a New Context finished

--- 018-food-order-app/src/components/Cart.jsx
+++ 018-food-order-app/src/components/Cart.jsx
@@ -1,12 +1,27 @@
 import { useContext } from "react";
 
 import Modal from "./UI/Modal";
+import Button from "../components/UI/Button.jsx";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
+import { currencyFormatter } from "../util/formatting.js";
+
 const Cart = () => {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
 
   return (
-    <Modal className="cart">
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
@@ -15,7 +30,13 @@ const Cart = () => {
           </li>
         ))}
       </ul>
-      <p className="cart-total"></p>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+      </p>
     </Modal>
   );
 };
--- 018-food-order-app/src/components/Header.jsx
+++ 018-food-order-app/src/components/Header.jsx
@@ -3,13 +3,19 @@ import React, { useContext } from "react";
 import Button from "./UI/Button";
 import logoImg from "../assets/logo.jpg";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 const Header = () => {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
   return (
     <header id="main-header">
       <div id="title">
@@ -17,7 +23,9 @@ const Header = () => {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
