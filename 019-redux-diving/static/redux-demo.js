dc00ab086534ab89f6d3706d21a89b938f7b1d88
Aleksandar Ristic <storiaca@gmail.com>
2024-11-30 15:26:25 +0100

    Expoliring Core Redux Concepts start

---
+++ 019-redux-diving/static/redux-demo.js
@@ -0,0 +1,16 @@
+import redux from "redux";
+
+const counterReducer = (state, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
