0f7d518e134e810190ba65e762bc82172c0b1a12
Aleksandar Ristic <storiaca@gmail.com>
2024-12-17 10:48:38 +0100

    Reading and dispatching from a New slice

--- 019-redux-diving/react/src/components/Header.js
+++ 019-redux-diving/react/src/components/Header.js
@@ -1,21 +1,34 @@
-import classes from './Header.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store";
+import classes from "./Header.module.css";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {auth && (
+          <ul>
+            <>
+              <li>
+                <a href="/">My Products</a>
+              </li>
+              <li>
+                <a href="/">My Sales</a>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </>
+          </ul>
+        )}
       </nav>
     </header>
   );
