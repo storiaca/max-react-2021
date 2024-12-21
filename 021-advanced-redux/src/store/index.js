b31b91e879e830375d442b017474456b030970ca
Aleksandar Ristic <storiaca@gmail.com>
2024-12-21 17:39:29 +0100

    Redux refresher practice 1

--- 021-advanced-redux/src/store/index.js
+++ 021-advanced-redux/src/store/index.js
@@ -1,3 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import uiSlice from "./ui-slice";
 
-configureStore({});
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer },
+});
+
+export default store;
