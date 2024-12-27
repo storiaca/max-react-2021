e428688f3201c8e003317ab4ee92b2a74421ac63
Aleksandar Ristic <storiaca@gmail.com>
2024-12-27 08:53:11 +0100

    Using useEffect with Redux

--- 021-advanced-redux/src/App.js
+++ 021-advanced-redux/src/App.js
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
+const FIREBASE_URL = process.env.REACT_APP_FIREBASE_URL;
+
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  console.log(FIREBASE_URL);
+
+  useEffect(() => {
+    fetch(`${FIREBASE_URL}/cart.json`, {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
