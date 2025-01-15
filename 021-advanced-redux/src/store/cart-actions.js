c4db03bdf854ea1e3cfd137c4addea0ffc0098a5
Aleksandar Ristic <storiaca@gmail.com>
2025-01-15 14:38:22 +0100

    Small fix

--- 021-advanced-redux/src/store/cart-actions.js
+++ 021-advanced-redux/src/store/cart-actions.js
@@ -21,6 +21,7 @@ export const fetchCartData = () => {
       dispatch(
         cartActions.replaceCart({
           items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
         })
       );
     } catch (error) {
