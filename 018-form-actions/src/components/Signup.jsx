d4997db8ce92ead3200797b926308b485a935945
Aleksandar Ristic <storiaca@gmail.com>
2025-01-18 17:07:48 +0100

    Adding Validation Checks

--- 018-form-actions/src/components/Signup.jsx
+++ 018-form-actions/src/components/Signup.jsx
@@ -1,7 +1,50 @@
+import {
+  isEmail,
+  isNotEmpty,
+  isEqualToOtherValue,
+  hasMinLength,
+} from "../util/validation.js";
+
 export default function Signup() {
   function signupAction(formData) {
-    const enteredEmail = formData.get("email");
-    console.log(enteredEmail);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm-password");
+    const firstName = formData.get("first-name");
+    const lastName = formData.get("last-name");
+    const role = formData.get("role");
+    const terms = formData.get("terms");
+    const acquisition = formData.getAll("acquisition");
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push("Please enter a valid email address.");
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push("You must provide a password with at least 6 characters.");
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push("Passwords do not match.");
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push("Please provide both your first and last name.");
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push("Please select a role.");
+    }
+
+    if (!terms) {
+      errors.push("You must agree to the terms and conditions.");
+    }
+
+    if (acquisition.length === 0) {
+      errors.push("Please tell us how you found us.");
+    }
   }
 
   return (
