8e1b8d3abcd9b800a94ffb435a7abf467b31c5f0
Aleksandar Ristic <storiaca@gmail.com>
2025-02-16 20:19:03 +0100

    Working with Asynchronous Form Actions

--- 018-form-actions/advanced/src/components/NewOpinion.jsx
+++ 018-form-actions/advanced/src/components/NewOpinion.jsx
@@ -1,7 +1,10 @@
-import { useActionState } from "react";
+import { useActionState, use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
 
 export function NewOpinion() {
-  function shareOpinionAction(prevState, formData) {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function shareOpinionAction(prevState, formData) {
     const userName = formData.get("userName");
     const title = formData.get("title");
     const body = formData.get("body");
@@ -25,6 +28,7 @@ export function NewOpinion() {
     }
 
     // submit the form to backend
+    await addOpinion({ userName, title, body });
 
     return { errors: null, enteredValues: { userName, title, body } };
   }
