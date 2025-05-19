3ff490290cc8f741938d69318f6b2f0e8b457253
Aleksandar Ristic <storiaca@gmail.com>
2025-05-19 09:03:07 +0200

    Changing Server to Client component

--- 027-rsc-actions/components/ClientDemo.js
+++ 027-rsc-actions/components/ClientDemo.js
@@ -1,5 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
 export default function ClientDemo({ children }) {
-  //const [count, setCount] = useState(0); // <- this is why it's a client component
+  const [count, setCount] = useState(0); // <- this is why it's a client component
 
   console.log("ClientDemo rendered");
   return (
@@ -7,6 +11,12 @@ export default function ClientDemo({ children }) {
       <h2>A React Client Component</h2>
       <p>
         Will be rendered on the client <strong>AND</strong> the server.
+      </p>
+      <p>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+          Increase
+        </button>
+        <span>{count}</span>
       </p>
       {children}
     </div>
