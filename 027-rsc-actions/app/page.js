1023ae791d44a5cc48d8127c7b9707ecf3a88b7d
Aleksandar Ristic <storiaca@gmail.com>
2025-05-21 16:21:54 +0200

    Using the use() hook for promises and Data fetching

--- 027-rsc-actions/app/page.js
+++ 027-rsc-actions/app/page.js
@@ -1,10 +1,15 @@
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
+import fs from "node:fs/promises";
+import { Suspense } from "react";
+import UsePromiseDemo from "@/components/UsePromisesDemo";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
-      <ServerActionsDemo />
+      <Suspense fallback={<p>Loading users...</p>}>
+        <UsePromiseDemo />
+      </Suspense>
     </main>
   );
 }
