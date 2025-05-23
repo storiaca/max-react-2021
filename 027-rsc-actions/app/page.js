a38c0f79f73adc2dc20b663deb55fcc72223f48f
Aleksandar Ristic <storiaca@gmail.com>
2025-05-23 20:32:01 +0200

    The use() Hook in action

--- 027-rsc-actions/app/page.js
+++ 027-rsc-actions/app/page.js
@@ -5,10 +5,18 @@ import { Suspense } from "react";
 import UsePromiseDemo from "@/components/UsePromisesDemo";
 
 export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
       <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo />
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
       </Suspense>
     </main>
   );
