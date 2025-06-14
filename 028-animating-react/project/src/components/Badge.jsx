3cff49bf9ba47a5d48ad08a0f7e790c6e3a832d9
Aleksandar Ristic <storiaca@gmail.com>
2025-06-14 13:47:18 +0200

    Re-triggering Animatins via keys

--- 028-animating-react/project/src/components/Badge.jsx
+++ 028-animating-react/project/src/components/Badge.jsx
@@ -1,3 +1,13 @@
+import { motion } from "framer-motion";
+
 export default function Badge({ caption }) {
-  return <span className="badge">{caption}</span>;
+  return (
+    <motion.span
+      initial={{ scale: [1, 1.2, 1] }}
+      transition={{ duration: 0.3 }}
+      className="badge"
+    >
+      {caption}
+    </motion.span>
+  );
 }
