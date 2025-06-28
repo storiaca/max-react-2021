c43ef7bfeb488130148cf552887044cefa111b1e
Aleksandar Ristic <storiaca@gmail.com>
2025-06-28 15:14:31 +0200

    sharing cross-component state start

--- 029-patterns/src/components/Accordion/AccordionItem.jsx
+++ 029-patterns/src/components/Accordion/AccordionItem.jsx
@@ -1,5 +1,14 @@
-const AccordionItem = ({ className, children }) => {
-  return <li className={className}>{children}</li>;
+import { createContext } from "react";
+
+const AccordionItemContext = createContext();
+
+export function useaccordionItemContext() {}
+const AccordionItem = ({ id, className, children }) => {
+  return (
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
+  );
 };
 
 export default AccordionItem;
