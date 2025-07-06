66fd09843d41ee06381c41ab52e99e23926aa511
Aleksandar Ristic <storiaca@gmail.com>
2025-07-06 13:32:02 +0200

    Alternative: using the Context APO

--- 030-redux-vs-hooks/src/index.js
+++ 030-redux-vs-hooks/src/index.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import App from "./App";
+import ProducsProvider from "./context/products-context";
+import "./index.css";
 
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
-
-const store = createStore(rootReducer);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <ProducsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProducsProvider>
 );
