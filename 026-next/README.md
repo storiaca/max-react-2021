c7d649e69246a7ae836a04f16f0205a6b90482db
Aleksandar Ristic <storiaca@gmail.com>
2025-07-23 20:54:18 +0200

    Reserved file names and custom components and how to organize a next project

---
+++ 026-next/README.md
@@ -0,0 +1,5 @@
+# Next.js Section
+
+## Organize files
+
+<https://nextjs.org/docs/app/getting-started/project-structure#colocation>
--- 026-next/app/page.js
+++ 026-next/app/page.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import Header from "@/components/header";
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <Header />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
