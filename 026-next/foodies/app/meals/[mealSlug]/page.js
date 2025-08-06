03cc7a655a4c1758fdb8a0cf6fcc8291a49b177b
Aleksandar Ristic <storiaca@gmail.com>
2025-08-06 23:00:13 +0200

    Loading & Rendering Meal Details via Dynamic Routes & Route Parameters start

--- 026-next/foodies/app/meals/[mealSlug]/page.js
+++ 026-next/foodies/app/meals/[mealSlug]/page.js
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
 export default function MealsDetailsPage() {
   return (
-    <main>
-      <h1>Meals Details</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <a href="">NAME</a>
+          </p>
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 }
